d8f7b48b402b6808cd97b63be612b36d99741648
Weishler <ilcreatore321@gmail.com>
2022-05-31 07:29:17 -0400

    Setting the structure
    packages added:
    + Tailwindcss
    + Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+// Redux
+import { Provider } from "react-redux";
+import store from "./app/store";
+
+import App from "./App";
+import "./styles/index.css";
+
+// React 18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-)
+  </Provider>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
